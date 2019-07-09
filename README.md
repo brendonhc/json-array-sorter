2ee62750af8e0ec7898ace9b9596dd3f7bfa2da9
Brendon <brendonhc@outlook.com>
2019-07-08 23:03:48 -0300

    Primeira versão

---
+++ README.md
@@ -0,0 +1,13 @@
+# JSON Array Sorter
+
+## Motivação
+
+Grandes arrays com objetos no formato JSON que precisam ser ordenados a partir de uma chave arbitrária como atributo comum em todos os objetos.
+
+## Como usar
+
+- Primeiro acesse o [aplicativo web do ordenador](brendonhc.github.io).
+- Insira o arr de objetos no formato JSON no primeiro campo.
+- Insira o nome do atributo que será usado como chave para a ordenação.
+- Clique no botão "SORT!".
+- O resultado será impresso no formato JSON logo abaixo do botão.
---
+++ script.js
@@ -0,0 +1,43 @@
+/**
+ * Função de ordenação do Brendon Hudson para arrays com objetos JavaScript
+ * 
+ * @param idProp é o id da propriedade usada para ordenar o vetor
+ * @author Brendon Hudson
+ */
+const hudSort = (idProp) => {
+
+}
+
+
+/**
+ * Controlador principal que é executado quando todos os componentes da página terminarem de carregar.
+ */
+window.onload = function () {
+
+  let listEl = document.getElementById("list");
+  let listIdPropEl = document.getElementById("listId");
+  let sortButtonEl = document.getElementById("listSortButton");
+  let resultEl = document.getElementById("result");
+
+  // Ouve os cliques no botão de ordenar
+  sortButtonEl.addEventListener("click", () => {
+
+    // Transformo o JSON de entrada em objeto JS
+    const list = JSON.parse(listEl.value);
+
+    // Se não preencher o campo, adotar _id como sendo padrão
+    if (listIdPropEl.value.length < 1) {
+      listIdPropEl.value = '_id';
+    }
+
+    // Ordeno a entrada do usuário
+    list.sort((a,b) => a[listIdPropEl.value] < b[listIdPropEl.value] ? -1 : 1)
+
+    // Imprimo o resultado
+    resultEl.innerHTML = JSON.stringify(list, undefined, 2);
+    // result.map(
+    //   item => resultEl.innerHTML = resultEl.innerHTML + `<li class="list-group-item">${item}</li>`
+    // );
+
+  });
+}
